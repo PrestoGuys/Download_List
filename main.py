65536e580017c634c15b7fa5a7cc288002332849
PrestoGuys <prestoguys@gmail.com>
2024-08-16 11:11:22 -0500

    added the code i had added test download

---
+++ main.py
@@ -0,0 +1,39 @@
+# Test 1 Link: https://ia800100.us.archive.org/28/items/test-image-1-for-download-list/Test.png
+
+# import modules
+import json
+import time
+import os
+
+# print opening message
+print('Download List v1.0')
+
+#
+with open('dl_list.json', 'r') as dllist:
+    dl_list = json.load(dllist)
+
+print('')
+print(dl_list)
+print('')
+
+dll_int = int(dl_list['dll'])
+dllstr = dl_list['dll']
+currdl = 1
+time.sleep(4)
+prompt = True
+
+
+while prompt is True:
+    user_input = input(']')
+
+    if user_input == 'run download':
+        while not (currdl > dll_int):
+            currdlstr = str(currdl)
+            dl_link = dl_list['dl' + currdlstr]
+
+            print('Download Link: ' + dl_link)
+            print('Download ID: ' + currdlstr)
+
+            os.system('axel ' + dl_link)
+
+            currdl += 1
